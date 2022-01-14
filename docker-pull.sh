8ff1a18908e6f0e006f42f093b217e33d516482c
vijay <yuanchongwei007@163.com>
2022-01-14 11:50:47 +0800

    ingress dockerfile

---
+++ docker-pull.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker pull registry.cn-qingdao.aliyuncs.com/vijayuan/kube-apiserver:v1.23.1
+docker pull registry.cn-qingdao.aliyuncs.com/vijayuan/kube-proxy:v1.23.1
+docker pull registry.cn-qingdao.aliyuncs.com/vijayuan/kube-scheduler:v1.23.1
+docker pull registry.cn-qingdao.aliyuncs.com/vijayuan/kube-controller-manager:v1.23.1
+docker pull registry.cn-qingdao.aliyuncs.com/vijayuan/etcd:3.5.1-0
+docker pull registry.cn-qingdao.aliyuncs.com/vijayuan/coredns:v1.8.6
+docker pull registry.cn-qingdao.aliyuncs.com/vijayuan/pause:3.6
+
